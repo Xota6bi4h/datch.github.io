4285e9e8da37dc55f5ae0a20fd87e1333bc7ddad
Stas <97909867+Xota6bi4h@users.noreply.github.com>
2023-02-05 15:14:06 +0400

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,35 @@
+const BADGES = [
+  {
+    name: "HypeSquad Balance", // Message that will appear when hovering over the badge
+    image: "assets/badges/hypesquad_balance.svg",
+  },
+  {
+    name: "Активный разработчик", // Message that appears when you hover over it
+    image: "assets/badges/normal_active_developer.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
+  },
+  {
+    name: "Первопроходец — верифицированный разработчик бота", // Message that appears when you hover over it
+    image: "assets/badges/verified_bot_developer.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
+  }
+];
+
+const USER_ID = "652483711564120095"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
+const BACKGROUND_IMAGE = "assets/images/star.gif"; // Here is the background of the site, behind your profile
+const STATUS = {
+  dnd: {
+    name: "🛑", // Discord Status
+    color: "#ed4245",
+  },
+  idle: {
+    name: "🌙",
+    color: "#faa81a",
+  },
+  online: {
+    name: "🍏",
+    color: "#3ba55d",
+  },
+  offline: {
+    name: "😴",
+    color: "#6e7985",
+  },
+};
