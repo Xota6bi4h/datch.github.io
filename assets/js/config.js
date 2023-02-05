c0aa680095ef4459bdf8eba651d05d76a9641eac
Stas <97909867+Xota6bi4h@users.noreply.github.com>
2023-02-05 21:07:44 +0400

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -14,7 +14,7 @@ const BADGES = [
 ];
 
 const USER_ID = "652483711564120095"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
-const BACKGROUND_IMAGE = "assets/images/star.gif"; // Here is the background of the site, behind your profile
+const BACKGROUND_IMAGE = "assets/images/snow-anime.gif"; // Here is the background of the site, behind your profile
 const STATUS = {
   dnd: {
     name: "🛑", // Discord Status
